f84d35fd601c2106f4bb8ee602f4c22490622f40
iwfan <i.wangfancn@gmail.com>
2019-12-19 17:04:29 +0800

    refactor(url serializer): use JSON.stringify pase array params

--- src/http_url_serializer.ts
+++ src/http_url_serializer.ts
@@ -1,54 +1,70 @@
-import { HttpParams } from './types';
+import { HttpParams } from './types'
 
-const EQUAL_SIGN = '=';
-const AMPERSAND_SIGN = '&';
-const QUESTION_SIGN = '?';
-const REGEXP_OF_STRING_AFTER_HASH = /#.*/;
+const EQUAL_SIGN = '='
+const AMPERSAND_SIGN = '&'
+const QUESTION_SIGN = '?'
+const REGEXP_OF_STRING_AFTER_HASH = /#.*/
 
-const isString = (val: any): val is string => typeof val === 'string';
+const assert = (condition: boolean, msg: string = 'Assertion Error') => {
+  if (!condition) {
+    throw new Error(msg)
+  }
+}
 
-const toString = (val: any) => isString(val) ? val : Object.prototype.toString.call(val);
+const toTypeString = (val: any) => Object.prototype.toString.call(val)
 
-const isArray = (val: any): val is [] => Array.isArray(val);
+const isString = (val: any): val is string => typeof val === 'string'
 
-const isObject = (val: any): val is object => val !== null && toString(val) === '[object Object]';
+const isArray = (val: any): val is any[] => Array.isArray(val)
 
-const isDate = (val: any): val is Date => toString(val) === '[object Date]';
+const isObject = (val: any): val is object =>
+  val !== null && toTypeString(val) === '[object Object]'
 
-const queryPairFor = (key: string, value: string) => `${encode(key)}${EQUAL_SIGN}${encode(value)}`;
+const isDate = (val: any): val is Date => toTypeString(val) === '[object Date]'
 
-const encode = (val: string) => encodeURIComponent(val)
-  .replace(/%20/g, '+')
-  .replace(/%(?:40|3A|24|2C|20|5B|5D|7B|7D)/ig, decodeURIComponent);
+const toString = (val: any) =>
+  isObject(val) || isArray(val) ? JSON.stringify(val) : `${val}`
 
+const queryPairFor = (key: string, value: string, isArray?: boolean) =>
+  `${encode(key)}${isArray ? '[]' : ''}${EQUAL_SIGN}${encode(value)}`
+
+const encode = (val: string) =>
+  encodeURIComponent(val)
+    .replace(/%20/g, '+')
+    .replace(/%(?:40|3A|24|2C|20|5B|5D|7B|7D|22)/gi, decodeURIComponent)
 
 export class HttpUrlSerializer {
-
   public serialize(url: string, params?: HttpParams): string {
+    assert(isString(url), '"url" is not a string')
 
-    if (!isString(url)) {
-      throw new TypeError('"url" is not a string');
-    }
-
-    url = url.replace(REGEXP_OF_STRING_AFTER_HASH, '');
+    url = url.replace(REGEXP_OF_STRING_AFTER_HASH, '')
 
     if (params) {
       const queryString = Object.entries(params)
         .map(([key, value]) => {
-          if (isDate(value)) { return queryPairFor(key, value.toISOString()); }
-          if (isObject(value)) { return queryPairFor(key, JSON.stringify(value)); }
-          if (isArray(value)) { return value.map(toString).map(str => queryPairFor(key, str)).join(AMPERSAND_SIGN); }
-          return queryPairFor(key, value as string);
+          if (isDate(value)) {
+            return queryPairFor(key, value.toISOString())
+          }
+          if (isObject(value)) {
+            return queryPairFor(key, JSON.stringify(value))
+          }
+          if (isArray(value)) {
+            return value
+              .map(toString)
+              .map(str => queryPairFor(key, str, true))
+              .join(AMPERSAND_SIGN)
+          }
+          return queryPairFor(key, value as string)
         })
         .reduce(
           (qs, queryPair) => `${qs}${queryPair}${AMPERSAND_SIGN}`,
           url.includes(QUESTION_SIGN) ? AMPERSAND_SIGN : QUESTION_SIGN
         )
-        .slice(0, -1);
+        .slice(0, -1)
 
-      url += queryString;
+      url += queryString
     }
 
-    return url;
+    return url
   }
 }
--- test/http_url_serializer.spec.ts
+++ test/http_url_serializer.spec.ts
@@ -1,60 +1,72 @@
-import { HttpUrlSerializer } from '../src/http_url_serializer';
+import { HttpUrlSerializer } from '../src/http_url_serializer'
 
 describe('As a UrlSerializer', () => {
-
-  const serializer = new HttpUrlSerializer();
+  const serializer = new HttpUrlSerializer()
 
   it('should throw exception when give url is not a string', () => {
-    expect(() => serializer.serialize(undefined as unknown as string))
-      .toThrowError(new TypeError('"url" is not a string'));
-  });
+    expect(() =>
+      serializer.serialize((undefined as unknown) as string)
+    ).toThrowError('"url" is not a string')
+  })
 
   it('should return raw url when given url is not includes hash', () => {
-    const url = 'http://localhost:3000/abc';
-    expect(serializer.serialize(url)).toBe(url);
-  });
+    const url = 'http://localhost:3000/abc'
+    expect(serializer.serialize(url)).toBe(url)
+  })
 
   it('should remove hash when given url includes hash', () => {
-    const url = 'http://localhost:3000/abc#def';
-    expect(serializer.serialize(url)).toBe('http://localhost:3000/abc');
-  });
+    const url = 'http://localhost:3000/abc#def'
+    expect(serializer.serialize(url)).toBe('http://localhost:3000/abc')
+  })
 
   it('should serialize the string params', () => {
-    const url = 'http://example.com';
-    expect(serializer.serialize(url, { foo: 'bar', baz: 'qux' }))
-      .toBe('http://example.com?foo=bar&baz=qux')
-  });
+    const url = 'http://example.com'
+    expect(serializer.serialize(url, { foo: 'bar', baz: 'qux' })).toBe(
+      'http://example.com?foo=bar&baz=qux'
+    )
+  })
 
   it('should serialize the array params', () => {
-    const url = 'http://example.com';
-    expect(serializer.serialize(url, { foo: ['bar', 'baz', 'qux'] }))
-      .toBe('http://example.com?foo=bar&foo=baz&foo=qux')
-  });
+    const url = 'http://example.com'
+    expect(serializer.serialize(url, { foo: ['bar', 'baz', 'qux'] })).toBe(
+      'http://example.com?foo[]=bar&foo[]=baz&foo[]=qux'
+    )
 
+    expect(
+      serializer.serialize(url, {
+        foo: ['bar', { foo: 'bar' }, [{ foo: 'bar' }]]
+      })
+    ).toBe(
+      'http://example.com?foo[]=bar&foo[]={"foo":"bar"}&foo[]=[{"foo":"bar"}]'
+    )
+  })
 
   it('should serialize the date params', () => {
-    const url = 'http://example.com';
-    const date = new Date();
-    expect(serializer.serialize(url, { foo: date }))
-      .toBe(`http://example.com?foo=${date.toISOString()}`)
-  });
+    const url = 'http://example.com'
+    const date = new Date()
+    expect(serializer.serialize(url, { foo: date })).toBe(
+      `http://example.com?foo=${date.toISOString()}`
+    )
+  })
 
   it('should serialize the object params', () => {
-    const url = 'http://example.com';
-    expect(serializer.serialize(url, { foo: { bar: 'baz', qux: 'qux' } }))
-      .toBe('http://example.com?foo={%22bar%22:%22baz%22,%22qux%22:%22qux%22}')
-  });
+    const url = 'http://example.com'
+    expect(serializer.serialize(url, { foo: { bar: 'baz', qux: 'qux' } })).toBe(
+      'http://example.com?foo={"bar":"baz","qux":"qux"}'
+    )
+  })
 
   it('should do not escape some special characters', () => {
-    const url = 'http://example.com';
-    expect(serializer.serialize(url, { foo: [new Object()], baz: '@:$, []' }))
-      .toBe(`http://example.com?foo=[object+Object]&baz=@:$,+[]`)
-  });
+    const url = 'http://example.com'
+    expect(
+      serializer.serialize(url, { foo: [{ qux: 'bar' }], baz: '@:$, []' })
+    ).toBe(`http://example.com?foo[]={"qux":"bar"}&baz=@:$,+[]`)
+  })
 
   it('should do not escape some special characters', () => {
-    const url = 'http://example.com#hash';
-    expect(serializer.serialize(url, { foo: ['bar'], baz: '@:$, []{}' }))
-      .toBe(`http://example.com?foo=bar&baz=@:$,+[]{}`)
-  });
-
-});
+    const url = 'http://example.com#hash'
+    expect(serializer.serialize(url, { foo: ['bar'], baz: '@:$, []{}' })).toBe(
+      `http://example.com?foo[]=bar&baz=@:$,+[]{}`
+    )
+  })
+})
