a2b770f8aee5ede114a3f1c4fb6318a23d66c4d4
iwfan <i.wangfancn@gmail.com>
2021-04-24 20:31:23 +0800

    refactor: generated infra

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/', '__test__', 'dist'],
+  collectCoverageFrom: ['src/*.{js,ts}']
+}
+
+export default config
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,37 +1,35 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
-
-const pkg = require('./package.json');
-
-const libraryName = 'happy_http';
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import pkg from './package.json'
 
 export default {
-  input: `lib/core/${libraryName}.ts`,
-  output: [
-    { file: pkg.main, name: 'HappyHttp', format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  input: './src/index.ts',
+
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+  // https://rollupjs.org/guide/en#external-e-external
   external: [],
   watch: {
-    include: 'lib/**',
+    include: 'src/**'
   },
   plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
+    // Allows node_modules resolution
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
 
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs({ extensions: ['.js', '.ts'] }),
+
+    // Compile TypeScript/JavaScript files
+    babel({ include: ['src/**/*'], runtimeHelpers: true }),
+
     // Resolve source maps to the original source
-    sourceMaps(),
+    sourceMaps()
   ],
-};
+
+  output: [
+    { file: pkg.main, name: 'HappyHttp', format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
+  ]
+}
